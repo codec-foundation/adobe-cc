aa0ef96b52781c932539455d9e30325c101ba86a
Tom Butterworth <bangnoise@gmail.com>
2019-10-10 10:51:23 +0100

    Mac run-time presets install - fix to create the Presets directory if it doesn't already exist

--- source/plugin/premiere/presets.mm
+++ source/plugin/premiere/presets.mm
@@ -74,12 +74,24 @@ void Presets::copy(const Presets::PathType &file, const Presets::PathType &sourc
         NSURL *source = [NSURL fileURLWithPath:[NSString stringWithUTF8String:source_dir.c_str()] isDirectory:YES];
         source = [source URLByAppendingPathComponent:[NSString stringWithUTF8String:file.c_str()]];
         NSURL *dst = [NSURL fileURLWithPath:[NSString stringWithUTF8String:destination_dir.c_str()] isDirectory:YES];
-        dst = [dst URLByAppendingPathComponent:[NSString stringWithUTF8String:file.c_str()]];
+        // Create the directory if it doesn't already exist
         BOOL exists = [[NSFileManager defaultManager] fileExistsAtPath:[dst path]];
-        if (replace && exists)
+        if (!exists)
         {
-            exists = ![[NSFileManager defaultManager] removeItemAtURL:dst error:nil];
+            exists = [[NSFileManager defaultManager] createDirectoryAtURL:dst withIntermediateDirectories:NO attributes:nil error:nil];
         }
+        // If the directory exists (or we successfully created it)
+        if (exists)
+        {
+            dst = [dst URLByAppendingPathComponent:[NSString stringWithUTF8String:file.c_str()]];
+            exists = [[NSFileManager defaultManager] fileExistsAtPath:[dst path]];
+            // If the file exists and we are to replace it, delete it
+            if (replace && exists)
+            {
+                exists = ![[NSFileManager defaultManager] removeItemAtURL:dst error:nil];
+            }
+        }
+        // If the file does not exist
         if (!exists)
         {
             // Ignoring result of this for now
